7d9a5600325f85e14eaa2cb59b27c44040c62814
devaniket16 <itsaniket.jsr@gmail.com>
2024-03-19 00:36:08 +0530

    commit

--- calculator.cpp
+++ calculator.cpp
@@ -2,36 +2,47 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     char op;
     double num1, num2;
-    cout << "Enter first number: ";
-    cin >> num1;
-    cout << "Enter second number: ";
-    cin >> num2;
-    cout << "Enter operation (+, -, *, /): ";
-    
-    cin >> op;
+    bool exitProgram = false;
 
-    switch(op) {
+    while (!exitProgram)
+    {
+        cout << "Enter first number: ";
+        cin >> num1;
+        cout << "Enter second number: ";
+        cin >> num2;
+        cout << "Enter operation (+, -, *, /) or 'q' to quit: ";
+        cin >> op;
+
+        switch (op)
+        {
         case '+':
-            cout << "Result: " << num1 + num2;
+            cout << "Result: " << num1 + num2 << endl;
             break;
         case '-':
-            cout << "Result: " << num1 - num2;
+            cout << "Result: " << num1 - num2 << endl;
             break;
         case '*':
-            cout << "Result: " << num1 * num2;
+            cout << "Result: " << num1 * num2 << endl;
             break;
         case '/':
             if (num2 != 0)
-                cout << "Result: " << num1 / num2;
+                cout << "Result: " << num1 / num2 << endl;
             else
-                cout << "Error! Division by zero!";
+                cout << "Error! Division by zero!" << endl;
+            break;
+        case 'q':
+            exitProgram = true;
             break;
         default:
-            cout << "Invalid operation!";
+            cout << "Invalid operation!" << endl;
+        }
     }
+
+    cout << "Exiting program. Goodbye!" << endl;
 
     return 0;
 }
